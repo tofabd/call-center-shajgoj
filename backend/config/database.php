aaa29e9ddd0e264870be29cc61022ac3e0efac99
tofabd <tofa.du@gmail.com>
2025-09-04 15:16:28 +0600

    Update database configuration and frontend environment settings
    - Modified database.php to dynamically set the host based on the operating system, defaulting to '10.255.255.254' for Linux and '127.0.0.1' for others.
    - Updated frontend .env file to uncomment and set the VITE_API_URL for Laravel API while keeping the MongoDB API URL commented.
    - Upgraded electron-to-chromium package version in package-lock.json from 1.5.213 to 1.5.214.

--- backend/config/database.php
+++ backend/config/database.php
@@ -45,7 +45,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            //'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', (PHP_OS_FAMILY === 'Linux') ? '10.255.255.254' : '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
